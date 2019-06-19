5d4fef1edec8a32e40eaaa7af93f2418095902a5
bottak <djbottak@gmail.com>
2019-06-19 17:37:38 +0300

    Initial Commit

---
+++ iOS-RecordVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-RecordVideo
+//
+//  Created by BOTTAK on 6/19/19.
+//  Copyright © 2019 BOTTAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
