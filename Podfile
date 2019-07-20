68cc8dddbc93a6361c3222d346c1248148775f2d
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-07-20 23:13:54 +0300

    added podfile and blocked warnings from pods

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+# ignore all warnings from all pods
+inhibit_all_warnings!
+
+target 'iOS-RecordVideo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'RealmSwift'
+  pod 'Kingfisher'
+  pod 'RealmSwift'
+  pod 'SwiftVideoGenerator'
+  pod 'LFLiveKit'
+
+end
---
+++ iOS-RecordVideo/Views/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  iOS-RecordVideo
+//
+//  Created by Владимир Королев on 20/07/2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIImagePickerController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
