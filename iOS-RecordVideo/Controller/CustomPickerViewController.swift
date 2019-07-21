251106efc2d51072d48669164b39e66a5b22a8c9
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-07-21 11:33:17 +0300

    pbxproj changes

---
+++ iOS-RecordVideo/Controller/CustomPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomPickerViewController.swift
+//  iOS-RecordVideo
+//
+//  Created by Владимир Королев on 21/07/2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+
+class CustomPickerViewController: UIImagePickerController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
