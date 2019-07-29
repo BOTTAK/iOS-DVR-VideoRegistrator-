c0ebe5c12194ae9c53bbdcf21ebe63aa991a1bdf
bottak <djbottak@gmail.com>
2019-07-29 17:51:57 +0300

    add imageUpload

---
+++ iOS-RecordVideo/Controller/ImageUploadViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ImageUploadViewController.swift
+//  iOS-RecordVideo
+//
+//  Created by BOTTAK on 7/29/19.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ImageUploadViewController: UIViewController {
+    
+    
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var imageViewUpload: UIImageView!
+    @IBOutlet weak var progressUploadToServer: UIProgressView!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var uploadButton: UIButton!
+    
+    //MARK: - Action
+    
+    @IBAction func uploadButtonToServer(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+       
+        
+        imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaInfo info: [UIImagePickerController.InfoKey : AnyObject]) {
+        imageViewUpload.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imageViewUpload.backgroundColor = UIColor.clear
+            self.dismiss(animated: true, completion: nil)
+            uploadImage()
+    }
+    
+    
+    func uploadImage() {
+        
+        let image = UIImage()
+        let imageDataSource = image.jpegData(compressionQuality: 1)
+        
+        if (imageDataSource == nil) { return
+            
+        }
+        
+        self.uploadButton.isEnabled = false
+        
+        let uploadScriptUrl = NSURL(string: "http://www.swiftdeveloperblog.com/http-post-example-script/")
+        let request = NSMutableURLRequest(url: uploadScriptUrl! as URL)
+        request.httpMethod = "POST"
+        request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+        
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration, delegate: self as? URLSessionDelegate, delegateQueue: OperationQueue.main)
+        
+        let task = session.uploadTask(with: request as URLRequest, from: imageDataSource!)
+        task.resume()
+        
+        
+        
+    }
+
+    func URLSessionTwo(session: URLSession, task: URLSessionTask, didCpmpleteWithError error: NSError?) {
+        
+        let myAlert = UIAlertView(title: "Alert", message: error?.localizedDescription, delegate: nil, cancelButtonTitle: "Ok")
+        myAlert.show()
+        self.uploadButton.isEnabled = true
+    }
+    
+    func URLSessionTree(session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedSend: Int64) {
+        
+        let uploadProgress: Float = Float(totalBytesSent) / Float(totalBytesExpectedSend)
+        
+        progressUploadToServer.progress = uploadProgress
+        let profressPercent = Int(uploadProgress*100)
+        progressLabel.text = "\(profressPercent)%"
+    }
+    
+    func URLSessionFour(session: URLSession, dataTask: URLSessionTask, didReceiveResponse response: URLResponse, complitionHandler: (URLSessionUploadTask) -> Void) {
+        
+        self.uploadButton.isEnabled = true
+        
+    }
+    
+}
--- iOS-RecordVideo/Model/ApiManager.swift
+++ iOS-RecordVideo/Model/ApiManager.swift
@@ -7,11 +7,15 @@
 //
 
 import Foundation
-//import Alamofire
+import Alamofire
+import Photos
 
 class ApiManager {
     
+    var video = VideoManager()
+    
     static var instance = ApiManager()
+    
     
     private enum Constants {
         static let baseURL = ""
@@ -27,7 +31,35 @@ class ApiManager {
     }
     
    
+   
+    
+    func uploadVideo(onComplete: @escaping ([VideoManager]) -> Void) { // local video file path..
+
+        let urlString = Constants.baseURL + EndPoints.uploads
+        
+//        Alamofire.upload(.POST, "http://test.com", multipartFormData: { (formData:MultipartFormData) in
+//            formData.appendBodyPart(fileURL: NSURL(fileURLWithPath: filePath), name: uname)
+//        }, encodingCompletion: { encodingResult in
+//            switch encodingResult {
+//            case .Success(let upload, _, _):
+//                upload.progress { bytesRead, totalBytesRead, totalBytesExpectedToRead in
+//                    print(totalBytesRead)
+//                }
+//                upload.responseJSON { response in
+//                    debugPrint(response)
+//                    //uploaded
+//                }
+//            case .Failure(let encodingError):
+//                //Something went wrong!
+//                if DEBUG_MODE {
+//                    print(encodingError)
+//                }
+//            }
+//        })
+        
+    }
     
     
+   
     
 }
