7767fdf3e95e3e3d7162e24bad332d856731ad81
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-07-31 15:33:04 +0300

    added info to networking manager

--- iOS-RecordVideo/Model/NetworkingManager.swift
+++ iOS-RecordVideo/Model/NetworkingManager.swift
@@ -18,6 +18,10 @@ struct APIConstants {
     static let login = "9@m.ru"
     static let password = "111111"
     static let expireDateString = "2019-12-01 12:00:00"
+    static let recordedAtConstString = "2019-06-01 12:00:00"
+    static let regNumber = "Х857ОР170RUS"
+    static let violationID = "1"
+    static let streetSide = "ODD"
     static let parameters: Parameters = ["login": APIConstants.login,
                                          "password": APIConstants.password,
                                          "expired_at": APIConstants.expireDateString]
@@ -51,14 +55,22 @@ final class NetworkingManager {
         //        let latitudeData = withUnsafeBytes(of: latitude) { Data($0) }
         //        let longtitudeData = withUnsafeBytes(of: longtitude) { Data($0) }
         //        let speedData = withUnsafeBytes(of: speed) { Data($0) }
+        let violationIDData = withUnsafeBytes(of: APIConstants.violationID) { Data($0) }
+        let regNumberData = withUnsafeBytes(of: APIConstants.regNumber) { Data($0) }
+        let streetSideData = withUnsafeBytes(of: APIConstants.streetSide) { Data($0) }
+        let recordedAtData = withUnsafeBytes(of: APIConstants.recordedAtConstString) { Data($0) }
         
         let httpHeaders = ["Authorization": "Bearer \(token)", "Cache-Control": "no-cache"]
-        
+        print(token)
         upload(multipartFormData: { (multipartFormData) in
             //            multipartFormData.append(latitudeData, withName: "location_data[latitude]")
             //            multipartFormData.append(longtitudeData, withName: "location_data[longitude]")
             //            multipartFormData.append(speedData, withName: "location_data[speed]")
-            //            multipartFormData.append(<#T##data: Data##Data#>, withName: "violations[0][violation_id]")
+            //            multipartFormData.append(, withName: "violations[0][violation_id]")w
+            multipartFormData.append(violationIDData, withName: "violations[0][violation_id]")
+            multipartFormData.append(regNumberData, withName: "violations[0][reg_number]")
+            multipartFormData.append(streetSideData, withName: "street_side")
+            multipartFormData.append(recordedAtData, withName: "recorded_at")
             multipartFormData.append(videoUrl, withName: "videoFile", fileName: "secondTry.mp4", mimeType: "video/mp4")
         }, to: APIConstants.uploadVideoURL,
            headers: httpHeaders) { (encodingCompletion) in
@@ -78,6 +90,7 @@ final class NetworkingManager {
                     switch responseJSON.result {
                     case .success(let value):
                         complitionHandler(.success(value))
+                        print(value)
                     case .failure(let error):
                         complitionHandler(.failure(error))
                     }
