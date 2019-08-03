6f8c7dcba23ad9993032a7277cb9333b1cd048c4
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-08-03 21:59:44 +0300

    video preview implemented

--- iOS-RecordVideo/Controller/ChooseAndUploadController.swift
+++ iOS-RecordVideo/Controller/ChooseAndUploadController.swift
@@ -10,24 +10,44 @@ import UIKit
 
 class ChooseAndUploadController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var fileNames: [String] {
+        guard let names = FileManager.getFiles()?.1 else { return [] }
+        return names
+    }
+    
+    var fileURLs: [URL] {
+        guard let urls = FileManager.getFiles()?.0 else { return [] }
+        return urls
+    }
+    
+    var choosenFile: URL?
 
-    @IBOutlet weak var videoPreviewView: UIView!
+    @IBOutlet weak var videoPreviewView: VideoPreview!
     @IBOutlet weak var videoListTableView: UITableView!
     @IBOutlet weak var uploadButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        videoListTableView.delegate = self
+        videoListTableView.dataSource = self
     }
     
 }
 
 extension ChooseAndUploadController {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return fileNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "videoCell", for: indexPath)
+        cell.textLabel?.text = fileNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        choosenFile = fileURLs[indexPath.row]
+        videoPreviewView.videoUrl = choosenFile
     }
 }
