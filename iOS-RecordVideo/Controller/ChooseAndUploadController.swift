10e2cca79c440191a7e5cdc66840f17684e97771
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-08-03 23:40:22 +0300

    table view library created and implemented'

--- iOS-RecordVideo/Controller/ChooseAndUploadController.swift
+++ iOS-RecordVideo/Controller/ChooseAndUploadController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ChooseAndUploadController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let networkingManager = NetworkingManager()
     
     var fileNames: [String] {
         guard let names = FileManager.getFiles()?.1 else { return [] }
@@ -31,13 +34,42 @@ class ChooseAndUploadController: UIViewController, UITableViewDelegate, UITableV
 
         videoListTableView.delegate = self
         videoListTableView.dataSource = self
+        networkingManager.authorisation { token in
+            self.networkingManager.token = token
+        }
+    }
+    
+    @IBAction func uploadPress(_ sender: UIButton) {
+        guard let urlToUpload = choosenFile else {
+            UIHelper.showError(errorMessage: "You must choose file to upload.", controller: self)
+            return
+        }
+        prepareFile(url: urlToUpload)
+    }
+    
+    func prepareFile(url: URL) {
+        let asset = AVURLAsset(url: url, options: [AVURLAssetPreferPreciseDurationAndTimingKey: true])
+        
+        guard let metadata = asset.metadata.first?.value else { fatalError() }
+        let newMetadata = String(metadata as! Substring)
+        
+        let parsedMetaDataArray = newMetadata.components(separatedBy: "+")
+        
+        networkingManager.uploadVideo(videoUrl: url, metadata: parsedMetaDataArray) { result in
+            switch result {
+            case let .success(value):
+                print(value as Any)
+            case let .failure(error):
+                UIHelper.showError(errorMessage: error.localizedDescription, controller: self)
+            }
+        }
     }
     
 }
 
 extension ChooseAndUploadController {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fileNames.count
+        return fileNames.count  
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
