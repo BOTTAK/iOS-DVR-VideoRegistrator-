875aa956ca8f2b0b90d13c8d0a2a4f742d07e485
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-08-03 21:59:23 +0300

    custom view for video preview been created

--- iOS-RecordVideo/Controller/VideoPreview.swift
+++ iOS-RecordVideo/Controller/VideoPreview.swift
@@ -7,15 +7,99 @@
 //
 
 import UIKit
+import AVFoundation
 
-class VideoPreview: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+public class VideoPreview: UIView {
+    public var videoUrl: URL? {
+        didSet {
+            if let _ = videoUrl {
+                self.playerItem = AVPlayerItem.init(url: self.videoUrl! as URL)
+            }
+        }
     }
-    */
-
+    @IBInspectable public var muted: Bool = true {
+        didSet {
+            self.player.isMuted = self.muted
+        }
+    }
+    public var player: AVPlayer!
+    public var playerItem: AVPlayerItem? {
+        didSet {
+            if let playerItem = self.playerItem {
+                player = AVPlayer(playerItem: playerItem)
+                player.actionAtItemEnd = .none
+                self.player.isMuted = self.muted
+                NotificationCenter.default.removeObserver(self)
+                NotificationCenter.default.addObserver(self,
+                                                       selector: #selector(playerItemDidReachEnd(notification:)),
+                                                       name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                                       object: playerItem)
+                self.playerLayer.player = self.player
+                self.player.play()
+                self.layoutSubviews()
+            }
+        }
+    }
+    public let playerLayer: AVPlayerLayer = AVPlayerLayer.init()
+    
+    // MARK:- View Lifecycle
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame);
+        
+        {
+            self.setup()
+        }()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+        
+        {
+            self.setup()
+        }()
+    }
+    
+    public init(videoUrl: URL) {
+        self.init();
+        
+        {
+            self.videoUrl = videoUrl;
+        }()
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // update AVPlayerLayer frame when VideoLoopView has been resized
+        var newFrame = self.frame
+        newFrame.origin = CGPoint(x: 0, y: 0)
+        self.playerLayer.frame = newFrame
+        self.playerLayer.removeFromSuperlayer()
+        self.layer.addSublayer(self.playerLayer)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func setup() {
+        self.playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+    }
+    
+    // MARK:- Controls
+    
+    @objc public func playVideo() {
+        self.player.play()
+    }
+    
+    @objc public func pauseVideo() {
+        self.player.pause()
+    }
+    
+    // MARK:- NSNotification
+    @objc func playerItemDidReachEnd(notification: NSNotification) {
+        let playerItem = notification.object as! AVPlayerItem
+        playerItem.seek(to: CMTime.zero)
+    }
 }
